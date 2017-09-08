a5d5fe5451b649890afc9b1181d813447364d512
Michael Patterson <mpatterson@pillartechnology.com>
2017-09-07 23:32:37 -0500

    First commit: basic starter project.

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Slichter's Jams</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
