bd5e847913e2187a12517cc7c54ab5caa4835b67
Troy Leyda <troy.leyda@gmail.com>
2017-10-18 00:53:42 -0500

    temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import * as React from 'react';
 import './App.css';
-import { JamInput } from './JamInput';
-import { JamText } from './JamText';
+// import { JamInput } from './JamInput';
+// import { JamText } from './JamText';
 
-const logo = require('./logo.svg');
+const logo = require('./full-white.svg');
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to Slichter's Jams</h2>
-                </div>
-                <div>
-                    <JamInput defaultMessage="test"/>
-                    <JamText/>
+                <div className="gradient">
+                    <img src={logo} className="full-white"/>
+                    <text className="let-s-jam">LET'S JAM</text>
+                    {/*<JamInput defaultMessage="test"/>*/}
+                    {/*<JamText/>*/}
                 </div>
             </div>
         );
