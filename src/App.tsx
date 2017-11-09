16579a8ec8de29c4b6210ba7782f6ffb4bd9229c
Michael Patterson <mjpatter88@gmail.com>
2017-11-09 00:01:31 -0600

    Basic layout and styling to match mockups.

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,35 @@ import { JamText } from './JamText';
 import { Flex } from 'react-flex';
 import 'react-flex/index.css';
 
-const logo = require('./logo.svg');
-const atom = require('./atom.png');
+const logo = require('./img/logo.svg');
+const atom = require('./img/atom.png');
 
 class App extends React.Component {
     render() {
         return (
-            <div className="App gradient">
-                <Flex justifyContent="space-between">
-                    <img src={logo}/>
-                    <img src={atom}/>
-                </Flex>
+            <div className="App">
+                <div id="header-bar">
+                    <Flex justifyContent="space-between">
+                        <div id="logo">
+                            <img src={logo}/>
+                        </div>
+                        <div id="atom">
+                            <img src={atom}/>
+                        </div>
+                    </Flex>
+                </div>
+
+                <div className="vert-spacer"/>
+
                 <Flex column={true} alignItems="center">
-                    <div>
-                        <text className="let-s-jam">LET'S JAM</text>
+                    <div className="jam-title">
+                        LET'S JAM
                     </div>
-                    <div>
-                        <text className="search-anything">Search anything in the world and see if it’s jam-worthy
-                            according to Slichter.
-                        </text>
+                    <div className="search-anything">
+                        Search anything in the world and see if it’s jam-worthy according to Slichter.
                     </div>
-                    <div className="text-input-rectangle">
-                        <JamInput defaultMessage="test"/>
+                    <div className="text-input">
+                        <JamInput defaultMessage="puppies"/>
                     </div>
                     <div className="search-rectangle">
                         <input className="search-button" type="button" value="Search"/>
