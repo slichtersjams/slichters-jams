1ce875211b9dbaea322db2ce80ab829ba11553ed
Tasewell Fox <tfox@pillartechnology.com>
2017-11-19 18:10:58 -0600

    add terrible styling for modal

--- src/components/jamModal/jamModal.tsx
+++ src/components/jamModal/jamModal.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import './jamModal.css';
 
 export class JamModal extends React.Component<IJamModalProps, IJamModalState> {
   constructor(props: IJamModalProps) {
@@ -12,8 +13,10 @@ export class JamModal extends React.Component<IJamModalProps, IJamModalState> {
     }
 
     return (
-      <div>
-        <img src={this.props.imgSrc} />
+      <div className="backdrop">
+        <div className="modal">
+          <img src={this.props.imgSrc} />
+        </div>
       </div>
     )
   }
