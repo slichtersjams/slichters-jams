84ab1395fad7e4a3e831773145fc9fe942d45a11
Tasewell Fox <tfox@pillartechnology.com>
2017-11-20 00:16:37 -0600

    call prop directly

--- src/components/jamModal/jamModal.tsx
+++ src/components/jamModal/jamModal.tsx
@@ -7,17 +7,13 @@ export class JamModal extends React.Component<IJamModalProps, IJamModalState> {
     this.state = {};
   }
 
-  public handleClick() {
-    this.props.close();
-  }
-
   public render() {
     if (!this.props.show) {
       return null;
     }
 
     return (
-      <div className="backdrop" onClick={e => this.handleClick()}>
+      <div className="backdrop" onClick={e => this.props.close()}>
         <div className="modal">
           <img src={this.props.imgSrc} />
         </div>
